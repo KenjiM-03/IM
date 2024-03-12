bf097d358282b39e4349bae97fb2f710a7476079
Joshua Sevilla <jSevilla@mcm.edu.ph>
2024-03-12 19:19:34 +0800

    PaySlip unfinish

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -6,7 +6,7 @@ public class DatabaseConnector {
 
     private static final String URL = "jdbc:mysql://localhost:3306/GEI";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "P4ssw0rd";
 
     static {
         try {
