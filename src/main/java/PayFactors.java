0f36aece6e2aa3b7bde9042c07e38731b3a9ccce
Joshua Sevilla <jSevilla@mcm.edu.ph>
2024-03-13 16:53:07 +0800

    sample

--- src/main/java/PayFactors.java
+++ src/main/java/PayFactors.java
@@ -146,12 +146,12 @@ public class PayFactors extends javax.swing.JFrame {
         //======== jPanel1 ========
         {
             jPanel1.setBackground(new Color(0x003333));
-            jPanel1.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border
-            .EmptyBorder(0,0,0,0), "JFor\u006dDesi\u0067ner \u0045valu\u0061tion",javax.swing.border.TitledBorder.CENTER,javax
-            .swing.border.TitledBorder.BOTTOM,new java.awt.Font("Dia\u006cog",java.awt.Font.BOLD,
-            12),java.awt.Color.red),jPanel1. getBorder()));jPanel1. addPropertyChangeListener(new java.beans
-            .PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("bord\u0065r".equals(e.
-            getPropertyName()))throw new RuntimeException();}});
+            jPanel1.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder
+            ( 0, 0 ,0 , 0) ,  "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border
+            .TitledBorder . BOTTOM, new java. awt .Font ( "Dialo\u0067", java .awt . Font. BOLD ,12 ) ,java . awt
+            . Color .red ) ,jPanel1. getBorder () ) ); jPanel1. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void
+            propertyChange (java . beans. PropertyChangeEvent e) { if( "borde\u0072" .equals ( e. getPropertyName () ) )throw new RuntimeException( )
+            ;} } );
 
             //---- jLabel2 ----
             jLabel2.setFont(new Font("Segoe UI", Font.BOLD, 24));
@@ -318,7 +318,6 @@ public class PayFactors extends javax.swing.JFrame {
 
             //---- Button_update ----
             Button_update.setText("Update");
-            Button_update.addActionListener(e -> updateRates());
 
             //---- Button_cancel ----
             Button_cancel.setText("Cancel");
