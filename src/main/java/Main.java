ffc3b8d1fe8b1665a77a44ae6c7f7474b4a03cfd
KenjiM-03 <147833354+KenjiM-03@users.noreply.github.com>
2024-03-15 16:01:58 +0800

    Partial Deletion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         try (Connection connection = DatabaseConnector.getConnection()) {
-            EmployeeRegistration.mainMenu(connection);
+            //EmployeeRegistration.mainMenu(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
